162b8592f19e110ff3eb43cb46a2833fbc89dd77
Dexalt142 <faturjackson14@gmail.com>
2021-05-15 19:41:05 +0700

    chore: initial commit

---
+++ app/Models/Pertemuan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pertemuan extends Model {
+
+    use HasFactory;
+
+    protected $table = 'pertemuan';
+
+    protected $fillable = [
+        'nama',
+        'tanggal',
+        'waktu',
+        'kode_pertemuan',
+        'guru_id'
+    ];
+    
+}
---
+++ database/migrations/2021_05_15_113238_create_presensi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensi', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->unsignedBigInteger('pertemuan_id');
+            $table->unsignedBigInteger('siswa_id');
+            $table->foreign('pertemuan_id')->references('id')->on('pertemuan');
+            $table->foreign('siswa_id')->references('id')->on('siswa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensis');
+    }
+}
