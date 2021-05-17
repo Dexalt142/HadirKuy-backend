fbfc47cf92d54619f1e10f8b59e890c3e2e9ea76
Dexalt142 <faturjackson14@gmail.com>
2021-05-17 22:39:48 +0700

    feat: server error message builder

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
+class Controller extends BaseController {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    /**
+     * Build internal server error message
+     *
+     * @param Exception $e
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorMessage(Exception $e) {
+        if(config(('app.debug'))) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Internal server error.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 500,
+            'message' => 'Internal server error.'
+        ], 500);
+    }
 }
