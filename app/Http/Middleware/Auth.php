3030d851858700d819013184f9949b49a35e62d1
Dexalt142 <faturjackson14@gmail.com>
2021-05-19 22:08:32 +0700

    feat: setup jwt

---
+++ app/Http/Middleware/Auth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class Auth {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next) {
+
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 400,
+                'message' => 'Unathorized.'
+            ], 400);
+        }
+
+        return $next($request);
+    }
+}
