8dcccd21e40a274a6d4d58f3475d35423f6cdfcc
Dexalt142 <faturjackson14@gmail.com>
2021-05-25 21:46:13 +0700

    feat: date format

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -31,5 +32,27 @@ class Controller extends BaseController {
             'status' => 500,
             'message' => 'Internal server error.'
         ], 500);
+    }
+
+    /**
+     * Format date
+     *
+     * @param String $tanggal
+     * @param String $waktu
+     * @param boolean $withTime
+     * @param boolean $withSeconds
+     * @return String
+     */
+    public function formattedDate($tanggal, $waktu, $withTime = false, $withSeconds = false) {
+        $dateTime = Carbon::parse("$tanggal $waktu");
+        if($withTime) {
+            if($withSeconds) {
+                return $dateTime->format('d M Y H:i:s');
+            }
+            
+            return $dateTime->format('d M Y H:i');
+        }
+        
+        return $dateTime->format('d M Y');
     }
 }
